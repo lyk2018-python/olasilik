002a0f500ff78911e62f248e36f566e0bb7535d4
Fatih Erikli <fatiherikli@gmail.com>
2018-07-22 12:24:25 +0300

    Init `olasilik.py`

---
+++ olasilik.py
@@ -0,0 +1,17 @@
+"""
+A tiny probability toolkit uses `random` module.
+
+  - dice_roll(how many of them)
+  - head_or_tail()
+  - rock_paper_scissors()
+"""
+
+def dice_roll(how_many):
+  return
+  
+def head_or_tail():
+  return
+  
+def rock_paper_scissors():
+  return
+ 
